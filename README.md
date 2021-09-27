37978afeec9f42824c4aa6dcaccd543cffdbe177
Abhinav Kumar <kumarat@rknec.edu>
2021-09-27 09:39:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Basic Python Projects
+
+Some basic python projects to practice python concepts through games
+
+- `madlibs.py`: madlibs! (practice with inputs, strings)
+- `rock_paper_scissors.py`: rock paper scissors (practice with functions and if statements)
+- `guessGame.py`: A simple number guess game, where either you can guess or computer can guess (Practice with loops and functions)
+
+
+Relevant YouTube videos:
+- https://youtu.be/8ext9G7xspg
---
+++ RockPaperScissors.py
@@ -0,0 +1,49 @@
+import random
+
+def play():
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer = isexist(user)
+    if not computer:
+        return  'Please Enter the letter correctly'
+    
+
+    if user == computer:
+        return 'It\'s a tie'
+
+    # r > s, s > p, p > r
+    check = is_win(user,computer)
+
+    if check:
+        return 'You won!'
+
+    if(user!=computer and not check and computer):
+         return 'You lost!'
+
+def is_win(player, opponent):
+    # return true if player wins and false if looses
+    # r > s, s > p, p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent == 'r'):
+        return True
+    return False
+
+def isexist(chance):
+    #check for correct letter entered by user or not 
+    #returns value if correct else returns false
+    tup = ('r','p','s')
+    if(chance in tup):
+        comp = random.choice(['r', 'p', 's'])
+        return comp
+    return False
+        
+
+flag = True
+print(play())
+while(flag):
+    repeat = input("Enter 'c' to continue or 'e' to exit: ").lower()
+    if(repeat=='c'):
+        print(play())
+    elif(repeat=='e'):
+        flag = False
+    else:
+        print("------Enter the choice correctly------")
